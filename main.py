26ae35689a14b5c44051151e839c96701d99402c
JuanMBuitrago <91805940+JuanMBuitrago@users.noreply.github.com>
2024-06-28 14:59:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -44,4 +44,4 @@ def recomendacion(prompt: str, marca: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
